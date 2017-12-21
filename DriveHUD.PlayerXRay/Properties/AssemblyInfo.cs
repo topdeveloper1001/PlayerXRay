fac58a71827dbb36361192038eb5cf6edb110b06
Alexander <aleksander.v.danilov@gmail.com>
2017-12-21 19:38:44 +0300

    Bumped to 1.0.0.1772

--- DriveHUD.PlayerXRay/Properties/AssemblyInfo.cs
+++ DriveHUD.PlayerXRay/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.1770")]
-[assembly: AssemblyVersion("1.0.0.1770")]
-[assembly: AssemblyFileVersion("1.0.0.1770")]
+// [assembly: AssemblyVersion("1.0.0.1772")]
+[assembly: AssemblyVersion("1.0.0.1772")]
+[assembly: AssemblyFileVersion("1.0.0.1772")]
 
 #if DEBUG
 
