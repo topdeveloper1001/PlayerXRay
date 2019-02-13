11692c51c4e28c6b71c06d28382370c8e9fee0ab
Alexander <aleksander.v.danilov@gmail.com>
2019-02-13 13:36:16 +0300

    Expiring soon message is shown 10 days before the expiration date (was 30)

--- DriveHUD.PlayerXRay/Licensing/LicenseInfo.cs
+++ DriveHUD.PlayerXRay/Licensing/LicenseInfo.cs
@@ -130,7 +130,7 @@ namespace DriveHUD.PlayerXRay.Licensing
         {
             get
             {
-                return IsRegistered && !IsTrial && TimeRemaining < TimeSpan.FromDays(30);
+                return IsRegistered && !IsTrial && TimeRemaining < TimeSpan.FromDays(10);
             }
         }
 
