6b4f44c2630eb1035d4407ab82d71d0621087f4c
Alexander <aleksander.v.danilov@gmail.com>
2019-02-14 23:28:27 +0300

    Bumped version to 1.0.0.1797

--- DriveHUD.PlayerXRay/Properties/AssemblyInfo.cs
+++ DriveHUD.PlayerXRay/Properties/AssemblyInfo.cs
@@ -50,9 +50,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.1794")]
-[assembly: AssemblyVersion("1.0.0.1794")]
-[assembly: AssemblyFileVersion("1.0.0.1794")]
+// [assembly: AssemblyVersion("1.0.0.1797")]
+[assembly: AssemblyVersion("1.0.0.1797")]
+[assembly: AssemblyFileVersion("1.0.0.1797")]
 
 #if DEBUG
 
--- XRHReg/Properties/AssemblyInfo.cs
+++ XRHReg/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.1794")]
-[assembly: AssemblyVersion("1.0.0.1794")]
-[assembly: AssemblyFileVersion("1.0.0.1794")]
+// [assembly: AssemblyVersion("1.0.0.1797")]
+[assembly: AssemblyVersion("1.0.0.1797")]
+[assembly: AssemblyFileVersion("1.0.0.1797")]
